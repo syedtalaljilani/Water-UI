0f6315641fb280d83101039ce141374912af1cf4
Syed Talal Jilani <talaljilani79@gmail.com>
2023-09-22 19:17:47 +0500

    Project Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import turbolinks from "@astrojs/turbolinks";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react({}), tailwind(), turbolinks()]
+});
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,8 @@
+const HeroSection = ()=>{
+    return(
+     <>
+     <h1 className="text-white text-6xl">Water UI{`----`}(HTML+TailWindCSS)</h1>
+     </>
+    );
+}
+export default HeroSection;
